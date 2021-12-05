4d56e53405bf69dcaf8339980b9bff0e25803386
krishnamohan1119 <krishnamohan.aake@gmail.com>
2021-12-05 14:31:08 +0530

    Update navigation.js

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -5,5 +5,14 @@ fetch('./../gallery/navigation.html')
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
+    var urlTotal= window.location.href;
+    for(var elements=document.getElementsByClassName('active'),i=0,l=elements.length; l>i; i++) {
+      elements[0].classList.remove("active");
+    }
+    if(urlTotal.search("index.html")>0){
+        document.getElementsById("index").classList.add('active');
+    }else if(urlTotal.search("about.html")>0){
+        document.getElementsById("index").classList.add('about');
+    }
 });
 
