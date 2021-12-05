eabe88a40ef9512c42adc6b336bf006be23a6125
Siva Kumar <63923267+shivakumarpeketi@users.noreply.github.com>
2021-12-05 12:03:05 +0530

    Update navigation.js

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,4 +1,4 @@
-function loadfetch('./../navigation.html')
+function fetch('./../navigation.html')
 .then(res => res.text())
 .then(text => {
     let oldelem = document.querySelector("script#navbar");
