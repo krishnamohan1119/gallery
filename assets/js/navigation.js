50aa6d28c4a9e4af48df8bbb21ce3417f107d7b8
krishnamohan1119 <krishnamohan.aake@gmail.com>
2021-12-05 14:08:17 +0530

    nav selection

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -5,4 +5,16 @@ fetch('./../gallery/navigation.html')
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
-})
+});
+let listItems = document.querySelectorAll('#nav a');
+
+listItems.forEach((item, index) => {
+  item.addEventListener('click', (event) => {
+	 for(var elements=document.getElementsByClassName('active'),i=0,l=elements.length; l>i; i++) {
+		  elements[0].classList.remove("active");
+		}
+	 var selected = event.currentTarget;
+	 selected.classList.add('active');
+	 
+  });
+});
