f33688b9d079c16a68b44c85cd830c1a5e586348
krishnamohan1119 <krishnamohan.aake@gmail.com>
2021-12-05 14:26:17 +0530

    Update navigation.js

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -6,15 +6,4 @@ fetch('./../gallery/navigation.html')
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
 });
-let listItems = document.querySelectorAll('#nav a');
 
-listItems.forEach((item, index) => {
-  item.addEventListener('click', (event) => {
-	 for(var elements=document.getElementsByClassName('active'),i=0,l=elements.length; l>i; i++) {
-		  elements[0].classList.remove("active");
-		}
-	 var selected = event.currentTarget;
-	 selected.classList.add('active');
-	 
-  });
-});
